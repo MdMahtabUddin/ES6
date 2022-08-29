574a5613ea27e05c2e4048242969c013485c0e46
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-08-30 00:55:15 +0600

    default parameter

--- default-parameter.js
+++ default-parameter.js
@@ -1,8 +1,16 @@
 // PH 31-4 Function default parameter for not provided values
-
-function numbers (num1, num2){
-console.log(num1, num2)
-
+// default parameter num2 =35  , when a parameter declare , default is not working
+function numbers (num1, num2 =35){
+// console.log(num1, num2)
+// before default parameter way-1
+num2 = num2 || 0;
+//before default parameter way-2
+// if (num2 == undefined){
+//     num2 = 0;
+// }
+const total =num1 +num2;
+return total;
 
 }
-numbers(52);
+const totalNumber =numbers(52, 58);
+console.log(totalNumber);
