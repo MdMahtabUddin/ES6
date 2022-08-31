98530249fb78b429160d0f9eeba84c89670a068f
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-08-31 12:28:54 +0600

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,6 +1,18 @@
 // PH 32-2 Destructuring Object to extract values to variables
 
-const fish ={ id: 45,name : 'hilsha', price: 2000, place: 'chandpur'};
+// const fish ={ id: 45,name : 'hilsha', price: 2000, place: 'chandpur'};
 
-console.log(fish.name);
-console.log(fish.id);
+// console.log(fish.name);
+// console.log(fish.id);
+
+
+// destructuring
+const company ={ name:'Gp',
+ ceo:{id:40, name:'mahtab'},
+ web:{work: 'web development', language:'javascript',framwork:'react'}
+};
+
+const {work, language,} =company.web;
+const {name, id} =company.ceo;
+
+console.log( name, work);
