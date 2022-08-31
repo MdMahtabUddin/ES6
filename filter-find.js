91f2a753345a2c0c77d325076841071377a71873
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-01 01:50:05 +0600

    filter-find

---
+++ filter-find.js
@@ -0,0 +1,7 @@
+// PH-32.6
+
+const numbers =[24, 10, 5, 15, 45, 27, 69];
+const bigNumber =numbers.filter(number => number > 20);
+console.log(bigNumber);
+const smallNumber =numbers.filter(number => number < 20);
+console.log(smallNumber);
