5dad6b0b64d1c01d0a88c18fbaae16286a39316c
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-01 01:24:24 +0600

    more-map

---
+++ more-map.js
@@ -0,0 +1,8 @@
+// PH 32-5 Map string array, array of objects map, foreach
+
+const friends =['Mahtab uddin', 'Mizbaur rahman','Jahir shawon','Monayeem hosain','Pavel khan'];
+
+// find a friends name length using map 
+
+const fLength =friends.map(friend => friend.length);
+console.log(fLength);
